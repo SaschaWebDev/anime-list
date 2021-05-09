66f1ae99277f861e7b7e53e34cea056c55d32785
Sascha Majewsky <sascha.majewsky@pm.me>
2021-05-09 18:23:05 +0200

    Added action dispatch

--- anime-list/src/app/containers/HomePage/index.tsx
+++ anime-list/src/app/containers/HomePage/index.tsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
+import { Dispatch } from "redux";
 import styled from "styled-components";
+import { useAppDispatch } from "../../hooks";
 import animeService from "../../services/animeService";
+import { GetAnimePage } from "../../services/animeService/__generated__/GetAnimePage";
+import { setAnimePage } from "./homePageSlice";
 
 interface IHomePageProps {}
 
@@ -12,13 +16,19 @@ const Container = styled.div`
   align-items: center;
 `;
 
+const actionDispatch = (dispatch: Dispatch) => ({
+  setAnimePage: (page: GetAnimePage["Page"]) => dispatch(setAnimePage(page)),
+});
+
 export function Homepage(props: IHomePageProps) {
+  const { setAnimePage } = actionDispatch(useAppDispatch());
   const fetchAnimePage = async () => {
     const animePage = await animeService.getAnimePage(0).catch((err) => {
       console.log("Error: ", err);
     });
 
     console.log("Anime page: ", animePage);
+    if (animePage) setAnimePage(animePage);
   };
 
   useEffect(() => {
